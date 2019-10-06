b8cce7ee7694852bcaf8372090d7f56f4d2a9e13
Edison Yap <edisonywh@gmail.com>
2019-10-06 22:58:35 +0800

    Add typespec and callbacks

--- lib/committee.ex
+++ lib/committee.ex
@@ -1,18 +1,28 @@
 defmodule Committee do
+  @hooks ~w(pre_commit post_commit)
+
   @moduledoc """
-  Documentation for Committee.
+  This module exposes a `__using__/1` that injects default implementation of git hooks
+  such as `#{Enum.join(@hooks, ", ")}`.
+
+  Users are expected to override them in `commit.exs` (created via `mix committee.install`).
   """
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Committee
 
-  @doc """
-  Hello world.
+      @doc false
+      @spec pre_commit() :: no_return
+      def pre_commit,  do: exit({:shutdown, 0})
 
-  ## Examples
+      @doc false
+      @spec post_commit() :: no_return
+      def post_commit, do: exit({:shutdown, 0})
 
-      iex> Committee.hello()
-      :world
-
-  """
-  def hello do
-    :world
+      defoverridable pre_commit: 0, post_commit: 0
+    end
   end
+
+  @callback pre_commit()  :: no_return
+  @callback post_commit() :: no_return
 end
