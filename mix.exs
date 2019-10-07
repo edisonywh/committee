7b8c1d3b044a717f94ece8e85bbfabeb073eaf27
Edison Yap <edisonywh@gmail.com>
2019-10-08 01:23:50 +0800

    Add `ex_doc` for dependency

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule Committee.MixProject do
     [
       app: :committee,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Committee",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/edisonywh/committee"
     ]
   end
 
@@ -21,8 +25,20 @@ defmodule Committee.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.21.2", only: :dev},
+      {:earmark, "~> 1.4", only: :dev}
+    ]
+  end
+
+  def description do
+    "⚡️Committee is a supercharged git hooks manager in pure Elixir"
+  end
+
+  def package do
+    [
+      maintainers: ["Edison Yap"],
+      licenses: ["MIT"],
+      links: %{GitHub: "https://github.com/edisonywh/committee"}
     ]
   end
 end
