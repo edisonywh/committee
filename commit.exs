703c7b8cd72ff01b57be9906958fa95b4c88f2f1
Edison Yap <edisonywh@gmail.com>
2019-10-09 02:00:55 +0800

    (Meta 🚀) Remove logging line
    With the previous commit, `Committee` will now log on hook start, so
    there's no need to write it in client's config.

--- commit.exs
+++ commit.exs
@@ -4,7 +4,6 @@ defmodule Committee.Commit do
 
   @impl true
   def pre_commit do
-    IO.puts("⚡️ Committee is running your `pre_commit` hook!\n")
     System.cmd("mix", ["format"] ++ staged_files([".ex", ".exs"]))
     System.cmd("git", ["add"] ++ staged_files())
   end
