dd7aef98805f24c346ec77aad186f3cce3b640d0
Edison Yap <edisonywh@gmail.com>
2019-10-09 01:42:59 +0800

    (Meta 🚀) Update config to lint `.ex & `.exs` only
    The previous commits added a new `staged_files/1` which allows for
    filter, this commit just updates Committee's own config script to use
    that. Dogfooding aye.

--- commit.exs
+++ commit.exs
@@ -1,10 +1,11 @@
 defmodule Committee.Commit do
   use Committee
-  import Committee.Helpers, only: [staged_files: 0]
+  import Committee.Helpers, only: [staged_files: 0, staged_files: 1]
 
   @impl true
   def pre_commit do
-    System.cmd("mix", ["format"] ++ staged_files())
+    IO.puts("⚡️ Committee is running your `pre_commit` hook!\n")
+    System.cmd("mix", ["format"] ++ staged_files([".ex", ".exs"]))
     System.cmd("git", ["add"] ++ staged_files())
   end
 end
