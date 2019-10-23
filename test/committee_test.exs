517a2cbe15f4f08f3d249c5d2bcd3dcb3800090d
Thiago Santos <thia.mdossantos@gmail.com>
2019-10-22 21:18:30 -0300

    Init tests

--- test/committee_test.exs
+++ test/committee_test.exs
@@ -1,7 +1,23 @@
 defmodule CommitteeTest do
   use ExUnit.Case
+  import Committee.TestHelpers
 
-  test "greets the world" do
-    assert true == true
+  test "greets the world", context do
+    in_tmp(context.test, fn ->
+      System.cmd("git", ["init"])
+      Mix.Tasks.Committee.Install.run([])
+
+      assert File.exists?(".committee.exs") == true
+
+      assert File.exists?(".git/hooks/pre-commit") == true
+      assert File.exists?(".git/hooks/post-commit") == true
+
+      assert executable?(".git/hooks/pre-commit") == true
+      assert executable?(".git/hooks/post-commit") == true
+    end)
   end
+
+  test "non git repo"
+  test ".committee.exs already exists"
+  test "existing hooks"
 end
