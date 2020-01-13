07625d454f4fdff7bfca7f03ef850658b06850a7
Edison Yap <edisonywh@gmail.com>
2020-01-13 22:21:33 +0100

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Committee.MixProject do
   def project do
     [
       app: :committee,
-      version: "0.2.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
